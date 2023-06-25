1e68dda85b77b59bd718172c2f81c9ca47cdc209
begi.dll <102294103+beeeegi@users.noreply.github.com>
2023-06-25 09:30:05 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", function () {
-        loaded();
-    });
+  document.addEventListener("DOMContentLoaded", function () {
+      loaded();
+  });
 } else if (document.attachEvent) {
-    document.attachEvent("onreadystatechange", function () {
-        loaded();
-    });
+  document.attachEvent("onreadystatechange", function () {
+      loaded();
+  });
 }
 
 function loaded() {
 
-    setInterval(loop, 600);
+  setInterval(loop, 600);
 
 }
 
 var x = 0;
 
 var titleText = [   "ㅤ",
-					"B",
-					"Be",
-					"Beg",
-					"Begi",
-					"Begi'",
-					"Begi's",
-					"Begi's B",
-                    "Begi's Ba",
-                    "Begi's Bas",
-                    "Begi's Base",
-                    "Begi's Basem",
-                    "Begi's Baseme",
-                    "Begi's Basemen",
-                    "Begi's Basement",
-                    "Begi's Basement",
-                    "Begi's Basemen",
-                    "Begi's Baseme",
-                    "Begi's Basem",
-                    "Begi's Base",
-                    "Begi's Bas",
-                    "Begi's Ba",
-                    "Begi's B",
-                    "Begi's",
-                    "Begi'",
-                    "Begi",
-                    "Beg",
-                    "Be",
-                    "B",
+        "B",
+        "Be",
+        "Beg",
+        "Begi",
+        "Begi'",
+        "Begi's",
+        "Begi's B",
+                  "Begi's Ba",
+                  "Begi's Bas",
+                  "Begi's Base",
+                  "Begi's Basem",
+                  "Begi's Baseme",
+                  "Begi's Basemen",
+                  "Begi's Basement",
+                  "Begi's Basement",
+                  "Begi's Basemen",
+                  "Begi's Baseme",
+                  "Begi's Basem",
+                  "Begi's Base",
+                  "Begi's Bas",
+                  "Begi's Ba",
+                  "Begi's B",
+                  "Begi's",
+                  "Begi'",
+                  "Begi",
+                  "Beg",
+                  "Be",
+                  "B",
 ];
 
 
 function loop() {
-    document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
+  document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
 }
 
 
@@ -65,23 +65,23 @@ let index = 0;
 let real = 1000;
 
 const rand = (min, max) => 
-  Math.floor(Math.random() * (max - min + 1)) + min;
+Math.floor(Math.random() * (max - min + 1)) + min;
 
 const animate = star => {
-  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
-  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
 
-  star.style.animation = "none";
-  star.offsetHeight;
-  star.style.animation = "";
+star.style.animation = "none";
+star.offsetHeight;
+star.style.animation = "";
 }
 
 for(const star of document.getElementsByClassName("magic-star")) {
-  setTimeout(() => {
-    animate(star);
-    
-    setInterval(() => animate(star), 1500);
-  }, index++ * (real / 3))
+setTimeout(() => {
+  animate(star);
+  
+  setInterval(() => animate(star), 1500);
+}, index++ * (real / 3))
 }
 
 
@@ -89,6 +89,31 @@ for(const star of document.getElementsByClassName("magic-star")) {
 
 
 document.querySelector(".begi").onmouseover = event => {  
+let iteration = 0;
+
+clearInterval(interval);
+
+interval = setInterval(() => {
+  event.target.innerText = event.target.innerText
+    .split("")
+    .map((letter, index) => {
+      if(index < iteration) {
+        return event.target.dataset.value[index];
+      }
+    
+      return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+  
+  if(iteration >= event.target.dataset.value.length){ 
+    clearInterval(interval);
+  }
+  
+  iteration += 1 / 3;
+}, 50);
+}
+
+document.querySelector(".mail").onmouseover = event => {  
   let iteration = 0;
   
   clearInterval(interval);
@@ -110,30 +135,5 @@ document.querySelector(".begi").onmouseover = event => {
     }
     
     iteration += 1 / 3;
-  }, 50);
+  }, 30);
 }
-
-document.querySelector(".mail").onmouseover = event => {  
-    let iteration = 0;
-    
-    clearInterval(interval);
-    
-    interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
-        .split("")
-        .map((letter, index) => {
-          if(index < iteration) {
-            return event.target.dataset.value[index];
-          }
-        
-          return letters[Math.floor(Math.random() * 26)]
-        })
-        .join("");
-      
-      if(iteration >= event.target.dataset.value.length){ 
-        clearInterval(interval);
-      }
-      
-      iteration += 1 / 3;
-    }, 30);
-  }
